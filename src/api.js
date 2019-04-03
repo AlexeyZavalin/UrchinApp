ba1a51308b4b1a36ac0ec90b14cf545eb7feb324
Alexey Zavalin <lehazavalin95@gmail.com>
2019-04-03 23:18:59 +1000

    Создано подключение к API, добавлен метод просмотра и добавления проекта

--- src/api.js
+++ src/api.js
@@ -1,7 +1,14 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: "http://127.0.0.1:8000/api/"
+    baseURL: "http://127.0.0.1:8000",
+    auth: {
+        username: "vue",
+        password: "vuejs"
+    }
+    // headers: {
+    //     "Authentication": "Bearer 172d42dfa46d92e19a824ce17400dc8b570985f2"
+    // }
 })
 
 export default instance;
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,14 +1,47 @@
 <template>
   <div class="component">
-    
+    <div class="project" v-for="project in projects" :key="project.slug">
+      {{ project.name }}
+      {{ project.description }}
+    </div>
+    <form @submit.prevent="createProject">
+      <input type="text" v-model="project.name" placeholder="Название"><br>
+      <input type="text" v-model="project.description" placeholder="Описание"><br>
+      <input type="text" v-model="project.date_start" placeholder="Дата начала"><br>
+      <input type="text" v-model="project.date_end" placeholder="Дата окончания"><br>
+      <input type="submit" value="Создать">
+    </form>
   </div>
 </template>
 
 <script>
+import instance from "@/api.js"
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      projects: [],
+      project: {
+        users: [],
+        slug: 'vuetest1',
+        name: '',
+        description: '',
+        date_start: '',
+        date_end: '',
+        is_active: true
+      }
+    }
+  },
+  mounted() {   
+    instance.get("/project/projects/")
+      .then(response => {
+        this.projects = response.data.results
+      })
+  },
+  methods: {
+    createProject() {
+      instance.post("/project/projects/", this.project)
+      .then(response => console.log(response))
+    }
   }
 }
 </script>
